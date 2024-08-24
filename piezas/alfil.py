94a6df269dc7d1c1f852c4e4d19f5ff3400f2b4c
Facundomesa <fa.mesa@alumno.um.edu.ar>
2024-08-23 22:11:41 -0300

    Alfil

---
+++ piezas/alfil.py
@@ -0,0 +1,32 @@
+class Bishop:
+    def __init__(self, color):
+        self.color = color
+
+    def __str__(self):
+        return f"{self.color} Bishop"
+
+    def get_valid_moves(self, position, board):
+        row, col = position
+        moves = []
+        directions = [
+            (-1, -1),  # Diagonal arriba a la izquierda
+            (-1, 1),   # Diagonal arriba a la derecha
+            (1, -1),   # Diagonal abajo a la izquierda
+            (1, 1)     # Diagonal abajo a la derecha
+        ]
+
+        for direction in directions:
+            r, c = row + direction[0], col + direction[1]
+            while 0 <= r < 8 and 0 <= c < 8:
+                piece = board[r][c]
+                if piece is None:
+                    moves.append((r, c))
+                elif piece.color != self.color:
+                    moves.append((r, c))
+                    break
+                else:
+                    break
+                r += direction[0]
+                c += direction[1]
+
+        return moves
---
+++ test_piezas/test_alfil.py
@@ -0,0 +1,45 @@
+import unittest
+
+class TestBishop(unittest.TestCase):
+    def test_bishop_initialization(self):
+        bishop = bishop("white")
+        self.assertEqual(bishop.color, "white")
+        self.assertEqual(str(bishop), "white Bishop")
+
+    def test_bishop_moves_from_center(self):
+        bishop = bishop("white")
+        board = [[None for _ in range(8)] for _ in range(8)]
+        position = (4, 4)
+        expected_moves = [
+            (3, 3), (2, 2), (1, 1), (0, 0),  # Diagonal arriba izquierda
+            (3, 5), (2, 6), (1, 7),          # Diagonal arriba derecha
+            (5, 3), (6, 2), (7, 1),          # Diagonal abajo izquierda
+            (5, 5), (6, 6), (7, 7)           # Diagonal abajo derecha
+        ]
+        self.assertCountEqual(bishop.get_valid_moves(position, board), expected_moves)
+
+    def test_bishop_moves_with_obstructions(self):
+        bishop = bishop("white")
+        board = [[None for _ in range(8)] for _ in range(8)]
+        board[2][2] = bishop("white")  # Obstrucción del mismo color
+        board[6][6] = bishop("black")  # Obstrucción de color contrario
+        position = (4, 4)
+        expected_moves = [
+            (3, 3),  # Diagonal arriba izquierda (parada antes del 2,2)
+            (3, 5), (2, 6), (1, 7),  # Diagonal arriba derecha
+            (5, 3), (6, 2), (7, 1),  # Diagonal abajo izquierda
+            (5, 5), (6, 6)           # Diagonal abajo derecha (parada en 6,6)
+        ]
+        self.assertCountEqual(bishop.get_valid_moves(position, board), expected_moves)
+
+    def test_bishop_moves_from_corner(self):
+        bishop = bishop("black")
+        board = [[None for _ in range(8)] for _ in range(8)]
+        position = (0, 0)
+        expected_moves = [
+            (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7)  # Diagonal única disponible
+        ]
+        self.assertCountEqual(bishop.get_valid_moves(position, board), expected_moves)
+
+if __name__ == '__main__':
+    unittest.main()
